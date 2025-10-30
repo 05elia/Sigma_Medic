297c0fd3681f9ec42c2f69becb5adf8be2409813
05elia <146644241+05elia@users.noreply.github.com>
2025-10-30 16:16:58 +0700

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,20 @@
 <template>
-  <footer class="mt-20">
+  <footer class="mt-0">
     <div class="bg-[#D7EAD1] text-gray-700 flex flex-col items-center py-10">
       <p class="text-sm mb-4 font-medium">
-        Dipercaya oleh rumah sakit dan klinik terkemuka di Indonesia
+        Dipercaya oleh Rumah Sakit dan Klinik Terkemuka di Indonesia
       </p>
       <div class="flex gap-8 flex-wrap justify-center items-center text-gray-600 font-semibold">
-        <span>Webflow</span>
-        <span>Relume</span>
-        <span>Webflow</span>
-        <span>Relume</span>
-        <span>Webflow</span>
-        <span>Relume</span>
+        <span>RS. Betha Medika</span>
+        <span>RS. Kartika Kasih</span>
+        <span>RSUD R. Syamsudin</span>
+        <span>RS. Sekar Wangi</span>
       </div>
     </div>
 
-    <div class="bg-[#A5C79B] text-gray-800 px-10 py-12 grid md:grid-cols-3 gap-8">
+    <div class="bg-[#A5C79B] text-gray-800 px-10 py-10 grid md:grid-cols-3 gap-8">
       <div>
-        <h2 class="text-3xl font-bold mb-3">Logo</h2>
+        <img src="/src/assets/logo.png" alt="Logo" class="h-30 w-auto mb-2"/>
       </div>
 
       <div>
@@ -31,7 +29,7 @@
         <h3 class="font-semibold mb-2 text-lg">Hubungi Kami</h3>
         <p class="text-sm mb-1">+62-8572-01882</p>
         <p class="text-sm mb-1">sigmamedic@gmail.com</p>
-        <p class="text-sm mb-1">jl skibid sigma boy</p>
+        <p class="text-sm mb-1">Jalan Cibolang Kaler Nomor </p>
       </div>
     </div>
 
